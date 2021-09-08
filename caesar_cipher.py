d75cc18754b27981e2a0f2b73e85d97e2e56efe0
Daniel Languiller <dlanguiller@opticomm.net.au>
2021-09-09 08:01:42 +1000

    permutations

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -8,6 +8,13 @@ def generate_key(n):
     return key
 
 
+def get_descryption_key(key):
+    dkey = {}
+    for c in key:
+        dkey[key[c]] = c
+    return dkey
+
+
 def encrypt(key, message):
     cipher = ""
     for c in message:
@@ -18,8 +25,15 @@ def encrypt(key, message):
     return cipher
 
 
+# This is done by your enemy:
 key = generate_key(3)
 print(key)
 message = "YOU ARE AWESOME"
 cipher = encrypt(key, message)
-print(cipher)
+
+# This is us breaking the cipher:
+# print(cipher)
+for i in range(26):
+    dkey = generate_key(i)
+    message = encrypt(dkey, cipher)
+    print(26-i, message)
