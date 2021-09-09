86877ed61c225ee93c44a98f26048503ca024421
Daniel Languiller <dlanguiller@opticomm.net.au>
2021-09-09 20:51:57 +1000

    sub_cipher

--- permutations.py
+++ permutations.py
@@ -1,11 +1,30 @@
-from itertools import permutations
+# from itertools import permutations
 
-my_list = [1, 2, 3, 4]
+# my_list = [1, 2, 3, 4]
 
-list_of_permutations = permutations(my_list)
+# list_of_permutations = permutations(my_list)
 
-cnt = 0
-for permutation in list_of_permutations:
-    cnt += 1
+# cnt = 0
+# for permutation in list_of_permutations:
+#     cnt += 1
 
-print(len(my_list), cnt)
+# print(len(my_list), cnt)
+
+import cProfile
+
+
+def faculty(n):
+    if n <= 1:
+        return n
+    else:
+        return faculty(n - 1) * n
+
+
+def counter(n):
+    count = 0
+    for i in range(n):
+        count += 1
+    return count
+
+
+cProfile.run("counter(faculty(12))")
